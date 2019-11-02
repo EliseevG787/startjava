b946f90da8895f0d9530653b0044e4aac8bad3ba
gennadiy <rdn787@bk.ru>
2019-11-03 01:53:50 +0300

    implement HW_1

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,26 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 7;
+        char sign = '^';
+
+        if (sign == '+') {
+            System.out.println(a + b);
+        } else if (sign == '-') {
+            System.out.println(a - b);
+        } else if (sign == '*') {
+            System.out.println(a * b);
+        } else if (sign == '/') {
+            System.out.println(a / b);
+        } else if (sign == '^') {
+            int result = 1;
+            for (int i = 1; i <= b; i++) {
+                result *= a;
+            }
+            System.out.println(result);
+        } else if (sign == '%') {
+            System.out.println(a % b);
+        }
+    }
+}
+
---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,17 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int randomNumber = 50;
+        int playerNumber = 1;
+
+        do {
+            if (playerNumber < randomNumber) {
+                System.out.println("Введенное вами число меньше того, что загадал компьютер");
+                playerNumber++;
+            } else if (playerNumber > randomNumber) {
+                System.out.println("Введенное вами число больше того, что загадал компьютер");
+                playerNumber--;
+            }
+        } while (playerNumber != randomNumber);
+        System.out.println("Вы угадали!");
+    }
+}
